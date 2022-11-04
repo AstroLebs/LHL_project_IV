5ff5e334bd1765b59dbb5a5fe7d1e62fe9ae672d
Caleb <calebgward@gmail.com>
2022-11-04 10:24:36 -0700

    Add requirements.txt

--- src/app/app.py
+++ src/app/app.py
@@ -1,9 +1,8 @@
 import pickle
 
-import numpy
 import pandas as pd
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
